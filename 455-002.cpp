8917129a9dd65e3a5192effaea7965364f654c68
Hrishikesh Gogoi <104480190+hrishikeshgogoi@users.noreply.github.com>
2024-01-24 17:42:46 +0530

    Add files via upload

---
+++ 455-002.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string x;
+    cout << "Enter datatype: ";
+    cin >> x;
+	transform(x.begin(),x.end(),x.begin(),::tolower);
+    int size;
+
+    if (x == "integer") size = 4;
+    else if (x == "long") size = 8;
+    else if (x == "float") size = 4;
+    else if (x == "double") size = 8;
+    else if (x == "character") size = 1;
+    else {
+        cout << "Unrecognized datatype: " << x << endl;
+        return 1;
+    }
+
+    cout << "Size is " << size << endl;
+    return 0;
+}
